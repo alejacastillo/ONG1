46e397377002ea95f07cddbcb380371974a9d3be
DELL <DELL@DESKTOP-PV5V4EA>
2021-02-16 21:46:31 -0600

    agregando mantenimiento al proyecto

--- ONG/ONG.BL/DesaparecidosBL.cs
+++ ONG/ONG.BL/DesaparecidosBL.cs
@@ -25,5 +25,43 @@ namespace ONG.BL
 
             return listaDesaparecidos;
           }
+
+
+        public void GuardarDesaparecido(Desaparecidos Desaparecidos)
+        {
+            if (Desaparecidos.Id == 0)
+            { 
+            _contexto.Desaparecido.Add(Desaparecidos);
+          
+            }else
+            {
+                var DesaparecidoExistente = _contexto.Desaparecido.Find(Desaparecidos.Id);
+                DesaparecidoExistente.PNombre = Desaparecidos.PNombre;
+                DesaparecidoExistente.SNombre = Desaparecidos.SNombre;
+                DesaparecidoExistente.PApellido = Desaparecidos.PApellido;
+                DesaparecidoExistente.SApellido = Desaparecidos.SApellido;
+                DesaparecidoExistente.Residencia = Desaparecidos.Residencia;
+                DesaparecidoExistente.Edad = Desaparecidos.Edad;
+                DesaparecidoExistente.Genero = Desaparecidos.Genero;
+
+            }
+
+            _contexto.SaveChanges();
+        }
+
+        public Desaparecidos ObtenerDesaparecidos( int id)
+        {
+            var Desaparecido = _contexto.Desaparecido.Find(id);
+            return Desaparecido;
+        }
+
+
+        public void EliminarDesaparecido(int id)
+        {
+            var Desaparecido = _contexto.Desaparecido.Find(id);
+
+            _contexto.Desaparecido.Remove(Desaparecido);
+            _contexto.SaveChanges();
+        }
     }
 }
---
+++ ONG/ONG.WebAdmin/Controllers/DesaparecidosController.cs
@@ -0,0 +1,83 @@
+﻿using ONG.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ONG.WebAdmin.Controllers
+{
+    public class DesaparecidosController : Controller
+    {
+
+        DesaparecidosBL _desaparecidosBL;
+
+        public DesaparecidosController()
+        {
+            _desaparecidosBL = new DesaparecidosBL();
+        }
+
+        // GET: Personas
+        public ActionResult Index()
+        {
+            var listadeDesaparecidos = _desaparecidosBL.ObtenerDesaparecidos();
+            return View(listadeDesaparecidos);
+        }
+
+    
+        public ActionResult Crear()
+        {
+
+            var nuevoDesaparecido = new Desaparecidos();
+
+            return View(nuevoDesaparecido);
+        }
+
+
+        [HttpPost]
+
+        public ActionResult Crear(Desaparecidos Desaparecidos)
+        {
+            _desaparecidosBL.GuardarDesaparecido(Desaparecidos);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Editar( int id)
+        {
+            var Desaparecidos = _desaparecidosBL.ObtenerDesaparecidos(id);
+
+            return View(Desaparecidos);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Desaparecidos Desaparecidos)
+        {
+            _desaparecidosBL.GuardarDesaparecido(Desaparecidos);
+
+            return RedirectToAction("Index");
+        }
+
+        public  ActionResult Detalle(int id)
+        {
+            var Desaparecidos = _desaparecidosBL.ObtenerDesaparecidos(id);
+            return View(Desaparecidos);
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            var Desaparecidos = _desaparecidosBL.ObtenerDesaparecidos(id);
+
+            return View(Desaparecidos);
+
+        }
+
+        [HttpPost]
+        public ActionResult  Eliminar(Desaparecidos Desaparecidos)
+        {
+            _desaparecidosBL.EliminarDesaparecido(Desaparecidos.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
